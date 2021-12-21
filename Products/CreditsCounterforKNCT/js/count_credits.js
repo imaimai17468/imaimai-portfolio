e4cc1e4cbbe4963cad7976f1edfb271696d137f7
imaimai17468 <tosiki_801_life@outlook.jp>
2021-12-21 23:56:00 +0900

    単位のカウントダウンを追加

--- Products/CreditsCounterforKNCT/js/count_credits.js
+++ Products/CreditsCounterforKNCT/js/count_credits.js
@@ -7,6 +7,8 @@ function credit_click(department){
     let detail_credit = document.getElementById("credit_detail_" + department_list_[department]);
     let required_credit = document.getElementById("credit_required_" + department_list_[department]);
     let credit_countdown = document.getElementById("credit_countdown_" + department_list_[department]);
+    let ippan_countdown = document.getElementById("ippan_countdown_" + department_list_[department]);
+    let senmon_countdown = document.getElementById("senmon_countdown_" + department_list_[department]);
 
     let credit_count = 0;
     let ippan = 0, senmon = 0, required = 0;
@@ -30,12 +32,24 @@ function credit_click(department){
     }
 
     all_credit.innerText = String(credit_count);
-    detail_credit.innerText = "一般科目 : " + String(ippan) + " 単位 / 専門科目 : " + String(senmon) + " 単位";
-    required_credit.innerText = "必修科目 : " + String(required) + " 単位";
+    detail_credit.innerText = "一般 : " + String(ippan) + " 単位 / 専門 : " + String(senmon) + " 単位";
+    required_credit.innerText = "必修 : " + String(required) + " 単位";
     
     if(167 - credit_count < 0){
-        credit_countdown.innerText = "167 単位から " + String(credit_count - 167)  + "単位余分に取得";
+        credit_countdown.innerText = "[全体] 167 単位から " + String(credit_count - 167)  + "単位余分に取得";
     }else{
-        credit_countdown.innerText = "167 単位まであと "  + String(167 - credit_count) + " 単位";
+        credit_countdown.innerText = "[全体] 167 単位まで残り "  + String(167 - credit_count) + " 単位";
+    }
+
+    if(75 - ippan < 0){
+        ippan_countdown.innerText = "[一般] 75 単位から " + String(ippan - 75)  + "単位余分に取得";
+    }else{
+        ippan_countdown.innerText = "[一般] 75 単位まで残り "  + String(75 - ippan) + " 単位";
+    }
+
+    if(82 - senmon < 0){
+        senmon_countdown.innerText = "[専門] 82 単位から " + String(senmon - 82)  + "単位余分に取得";
+    }else{
+        senmon_countdown.innerText = "[専門] 82 単位まで残り "  + String(82 - senmon) + " 単位";
     }
 }
--- Products/CreditsCounterforKNCT/js/output_counter.js
+++ Products/CreditsCounterforKNCT/js/output_counter.js
@@ -7,10 +7,11 @@
         insertElement += `<p>現在の合計単位数</p>`;
         insertElement += `<h1 id="credit_count_${department_list[i]}">0</h1>`;
         insertElement += `<p>単位</p>`;
-        insertElement += `<p id="credit_detail_${department_list[i]}">一般科目 : 0 単位 / 専門科目 : 0 単位</p>`;
-        insertElement += `<p id="credit_required_${department_list[i]}">必修科目 : 0 単位</p>`;
-        insertElement += `<p id="credit_countdown_${department_list[i]}">167 単位まであと 167 単位</p>`;
-
+        insertElement += `<p id="credit_detail_${department_list[i]}">一般 : 0 単位 / 専門 : 0 単位</p>`;
+        insertElement += `<p id="credit_required_${department_list[i]}">必修 : 0 単位</p>`;
+        insertElement += `<p id="credit_countdown_${department_list[i]}">[全体] 167 単位まで残り 167 単位</p>`;
+        insertElement += `<p id="ippan_countdown_${department_list[i]}">[一般] 75 単位まで残り 75 単位</p>`;
+        insertElement += `<p id="senmon_countdown_${department_list[i]}">[専門] 82 単位まで残り 82 単位</p>`;
         outputElement[i].innerHTML = insertElement;
     }
 }
