5f6e77cce2b319d1a4ef706ea09a227866cbfb21
imaimai17468 <tosiki_801_life@outlook.jp>
2022-03-17 12:43:38 +0900

    プロフィールページの制作

---
+++ Products/ange_matome/about/js/btn.js
@@ -0,0 +1,47 @@
+$('#normal-btn').click(function () {
+    const top = $('#normal').offset().top;
+    $('body,html').animate({
+        scrollTop: top
+    }, 500);
+    return false;
+});
+
+$('#summer-btn').click(function () {
+    const top = $('#summer').offset().top;
+    $('body,html').animate({
+        scrollTop: top
+    }, 500);
+    return false;
+});
+
+$('#dress-btn').click(function () {
+    const top = $('#dress').offset().top;
+    $('body,html').animate({
+        scrollTop: top
+    }, 500);
+    return false;
+});
+
+$('#idle-btn').click(function () {
+    const top = $('#idle').offset().top;
+    $('body,html').animate({
+        scrollTop: top
+    }, 500);
+    return false;
+});
+
+$('#hakui-btn').click(function () {
+    const top = $('#hakui').offset().top;
+    $('body,html').animate({
+        scrollTop: top
+    }, 500);
+    return false;
+});
+
+$('#val-btn').click(function () {
+    const top = $('#val').offset().top;
+    $('body,html').animate({
+        scrollTop: top
+    }, 500);
+    return false;
+});
