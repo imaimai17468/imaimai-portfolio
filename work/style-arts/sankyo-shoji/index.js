7f9c45b39ad1ed1b4caec49461fa4756e8caed37
Toshiki Imai <tosiki_801_life@outlook.jp>
2022-09-21 22:24:18 +0900

    fix url, debug, delete event[0]

--- work/style-arts/sankyo-shoji/index.js
+++ work/style-arts/sankyo-shoji/index.js
@@ -138,7 +138,7 @@ function subForm() {
               "body": JSON.stringify(data),
             };
 
-            const url = "https://script.google.com/macros/s/AKfycbxQnXSsA4cbCE4ivunogX0L1aX3mbCnjeH-S-ay5Q5H17ud0q9z9kGEyuSleiMeSwy4/exec";
+            const url = "https://script.google.com/macros/s/AKfycbyl_N1BVijUAP-czil_YGArBaVlNMZJXvDuhACYYAyJiER-bmBiPCKYOGXr2B-aB1AC/exec";
             fetch(url, postparam);
 
             console.log(postparam);
