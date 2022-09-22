ccf1ee60b48dc1363cfa9303b5cb267f99520f24
toshiki imai <tosiki_801_life@outlook.jp>
2022-09-23 00:50:21 +0900

    add debug in html

--- work/style-arts/sankyo-shoji/index.js
+++ work/style-arts/sankyo-shoji/index.js
@@ -154,12 +154,13 @@ function previewFile(index) {
     const files = document.querySelectorAll('input[type=file]');
     const file = files[index].files[0];
     const reader = new FileReader();
-
     reader.addEventListener("load", function () {
         // 画像ファイルを base64 文字列に変換します
         base64Texts[index] = reader.result;
         console.log(base64Texts);
         console.log(files);
+        let debug = document.getElementById('debug');
+        debug.innerHTML = `${base64Texts[0]}`;
     }, false);
 
     if (file) {
