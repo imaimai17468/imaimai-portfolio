427a75ff5fc51942bb789872c5beea31e5453c77
imaimai17468 <tosiki_801_life@outlook.jp>
2023-02-07 13:37:05 +0900

    [del] delete message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,20 +12,6 @@ export default function Home() {
         <link rel="icon" href="/images/frog_circle.png" />
       </Head>
       <main className="text-gray-200 font-mono">
-        <div className="border-2 m-5 rounded-md border-gray-200 w-fit h-fit p-5 shadow-lg shadow-gray-200">
-          <p className="text-3xl">Replacing Now... </p>
-          <p className="text-3xl">
-            Please watch my{" "}
-            <a
-              className="font-bold text-orange-300"
-              href="https://www.wantedly.com/id/imaimai17468"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              wantedly page
-            </a>
-          </p>
-        </div>
         <Playlist />
       </main>
     </>
