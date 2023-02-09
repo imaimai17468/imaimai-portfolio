0c2d56ec3d02548994856ab6e3c4345c803fe42a
imaimai17468 <tosiki_801_life@outlook.jp>
2023-02-09 18:35:35 +0900

    [feat] add open, click, settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,53 @@
 import Head from "next/head";
+import { useState, useMemo, useCallback } from "react";
 
+import { SideNavi } from "@/components/screen";
 import { Playlist } from "@/components/screen";
 import { Drag } from "@/components/Layout";
 import { useWindowSize } from "@react-hook/window-size";
 
 export default function Home() {
   const [windowWidth, windowHeight] = useWindowSize();
+  const [openMusic, setOpenMusic] = useState(false);
+  const [openAbout, setOpenAbout] = useState(false);
+  const [openWorks, setOpenWorks] = useState(false);
+  const [openSkills, setOpenSkills] = useState(false);
+
+  const toggleMusic = useCallback(() => {
+    setOpenMusic(!openMusic);
+  }, [openMusic]);
+
+  const toggleAbout = useCallback(() => {
+    setOpenAbout(!openAbout);
+  }, [openAbout]);
+
+  const toggleWorks = useCallback(() => {
+    setOpenWorks(!openWorks);
+  }, [openWorks]);
+
+  const toggleSkills = useCallback(() => {
+    setOpenSkills(!openSkills);
+  }, [openSkills]);
+
+  const onClicks = useMemo(
+    () => ({
+      onMusicClick: toggleMusic,
+      onAboutClick: toggleAbout,
+      onWorksClick: toggleWorks,
+      onSkillsClick: toggleSkills,
+    }),
+    [toggleAbout, toggleMusic, toggleSkills, toggleWorks]
+  );
+
+  const isOpens = useMemo(
+    () => ({
+      isOpenMusic: openMusic,
+      isOpenAbout: openAbout,
+      isOpenWorks: openWorks,
+      isOpenSkills: openSkills,
+    }),
+    [openAbout, openMusic, openSkills, openWorks]
+  );
 
   return (
     <>
@@ -16,12 +58,13 @@ export default function Home() {
         <link rel="icon" href="/images/frog_circle.png" />
       </Head>
       <main className="text-gray-200 font-mono">
+        <SideNavi onClicks={onClicks} isOpens={isOpens} />
         <Drag
           container={{
-            left: - windowWidth / 10,
-            top: - windowHeight / 10,
+            left: -windowWidth / 10,
+            top: -windowHeight / 10,
             right: windowWidth - windowWidth / 10,
-            bottom: windowHeight - windowHeight / 10 ,
+            bottom: windowHeight - windowHeight / 10,
           }}
           className="collapse sm:visible"
         >
