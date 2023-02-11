b93e7d1f42a88496bf41b47232417d006e6b0e37
imaimai17468 <tosiki_801_life@outlook.jp>
2023-02-11 15:55:00 +0900

    [feat] add work modal

---
+++ src/components/screen/WorkWindow/WorkWindow.tsx
@@ -0,0 +1,18 @@
+import { WorkWindowProps } from "./WorkWindow.type";
+import { Modal } from "@/components/common";
+
+export const WorkWindow: React.FC<WorkWindowProps> = ({
+  isOpen,
+  onClose,
+}) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} title="WORKS">
+      <div className="text-gray-200 flex flex-col ">
+        <p>This is Skill Page</p>
+        <p>Coming Soon...</p>
+      </div>
+    </Modal>
+  );
+};
+
+export default WorkWindow;
---
+++ src/components/screen/WorkWindow/WorkWindow.type.ts
@@ -0,0 +1,4 @@
+export interface WorkWindowProps {
+    onClose: () => void;
+    isOpen: boolean;
+}
