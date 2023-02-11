3a573a45088cddbc18b56731b2f5d05640cee529
imaimai17468 <tosiki_801_life@outlook.jp>
2023-02-11 15:54:01 +0900

    [fix] fix z-index

--- src/components/common/SideTab/SideTab.tsx
+++ src/components/common/SideTab/SideTab.tsx
@@ -6,7 +6,7 @@ export const SideTab = (props: SideTabProps) => {
 
   return (
     <div
-      className={`z-30 absolute
+      className={`z-20 absolute
         ${className}`}
       onClick={() => {onClick && onClick()}}
     >
