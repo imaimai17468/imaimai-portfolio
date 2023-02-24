d50e8acc82985faf64fa7d7316fb8bb41b6dc33f
imaimai17468 <tosiki_801_life@outlook.jp>
2023-02-24 22:03:20 +0900

    [fix] モーダルの色を戻す

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -3,12 +3,12 @@ import { ModalProps } from './Modal.type'
 export const Modal = ({ children, isOpen, title, onClose }: ModalProps) => (
   <div
     className={`
-         absolute z-10 h-fit w-72 translate-y-1/2 rounded-md border-2 border-accent bg-primary p-3 shadow-md shadow-accent sm:w-1/2 md:translate-x-1/2
+         absolute z-10 h-fit w-72 translate-y-1/2 rounded-md border-2 border-gray-200 bg-slate-800 p-3 shadow-md shadow-gray-200 sm:w-1/2 md:translate-x-1/2
         ${isOpen ? 'opacity-100' : 'hidden opacity-0'}
     `}
   >
     <div className='mb-3 flex flex-row items-center gap-3 border-b-2 pb-2'>
-      <button type='button' className='flex w-6 justify-center rounded-md border-2 border-accent' onClick={onClose}>
+      <button type='button' className='flex w-6 justify-center rounded-md border-2 border-gray-200' onClick={onClose}>
         <span className='mx-auto cursor-pointer' onClick={onClose}>
           ×
         </span>
