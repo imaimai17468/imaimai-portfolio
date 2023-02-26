ea654e3e5dd3bc4625c654c0a59d75e647629c5a
imaimai17468 <tosiki_801_life@outlook.jp>
2023-02-26 14:43:07 +0900

    [fix] fix modal design

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -3,19 +3,23 @@ import { ModalProps } from './Modal.type'
 export const Modal = ({ children, isOpen, title, onClose }: ModalProps) => (
   <div
     className={`
-         absolute z-10 h-fit w-72 translate-y-1/2 rounded-md border-2 border-gray-200 bg-slate-800 p-3 shadow-md shadow-gray-200 sm:w-1/2 md:translate-x-1/2
+         absolute z-10 h-fit w-72 translate-y-1/2 rounded-md border border-gray-200 bg-slate-800 px-3 py-1 shadow-md shadow-gray-200 sm:w-1/2 md:translate-x-1/2
         ${isOpen ? 'opacity-100' : 'hidden opacity-0'}
     `}
   >
-    <div className='mb-3 flex flex-row items-center gap-3 border-b-2 pb-2'>
-      <button type='button' className='flex w-6 justify-center rounded-md border-2 border-gray-200' onClick={onClose}>
-        <span className='mx-auto cursor-pointer' onClick={onClose}>
+    <div className='mb-1 flex flex-row items-center gap-3 border-b pb-1'>
+      <button
+        type='button'
+        className='flex h-4 w-4 items-center justify-center rounded-md border border-gray-200'
+        onClick={onClose}
+      >
+        <span className='mx-auto cursor-pointer text-sm ' onClick={onClose}>
           ×
         </span>
       </button>
-      <p className='text-xl'>{title}</p>
+      <p className='text-sm'>{title}</p>
     </div>
-    <div className='modal__content'>{children}</div>
+    <div className='p-3'>{children}</div>
   </div>
 )
 
