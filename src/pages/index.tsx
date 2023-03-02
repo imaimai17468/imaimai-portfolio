ddf9508e90bbdac0c58754b3a716a41dca782b3e
imaimai17468 <tosiki_801_life@outlook.jp>
2023-03-03 03:05:15 +0900

    [del] delete react animated cursor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,8 @@
-import dynamic from 'next/dynamic'
 import Head from 'next/head'
 import { useState, useMemo, useCallback } from 'react'
 
 import { Drag, MainLayout } from '@/components/layout'
 import { SideNavi, Playlist, BackgroundAnimation, AboutWindow, SkillWindow, WorkWindow } from '@/components/screen'
-
-const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
-  ssr: false,
-})
 
 export default function Home() {
   const [openMusic, setOpenMusic] = useState(false)
@@ -61,7 +56,6 @@ export default function Home() {
       </Head>
       <main className='font-mono text-gray-200'>
         <MainLayout>
-          <AnimatedCursor />
           <BackgroundAnimation />
           <SideNavi onClicks={onClicks} isOpens={isOpens} />
           <Drag>
