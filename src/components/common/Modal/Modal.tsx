3742577a87cd02545c781c0dd0d314ebdcf4fb60
imaimai17468 <tosiki_801_life@outlook.jp>
2023-03-10 23:36:13 +0900

    [fix] 位置とサイズ調整

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -3,7 +3,7 @@ import { ModalProps } from './Modal.type'
 export const Modal = ({ children, isOpen, title, onClose }: ModalProps) => (
   <div
     className={`
-         md:max-w-2/3 absolute z-10 h-fit w-fit translate-x-1/2 rounded-md border border-gray-200 bg-slate-800 px-3 py-1 shadow-md shadow-gray-200
+         md:max-w-2/3 absolute left-1/2 z-10 h-fit w-9/10 -translate-x-1/2 rounded-md border border-gray-200 bg-slate-800 px-3 py-1 shadow-md shadow-gray-200
         ${isOpen ? 'opacity-100' : 'hidden opacity-0'}
     `}
   >
