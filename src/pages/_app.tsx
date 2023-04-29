e3df990ce201c1945fed64017dd77bf28c3b23f4
imaimai17468 <tosiki_801_life@outlook.jp>
2023-04-29 23:03:19 +0900

    [feat] add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import '@/styles/globals.css'
+import { Analytics } from '@vercel/analytics/react'
 import { RecoilRoot } from 'recoil'
 
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <>
+      <Analytics />
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </>
   )
 }
