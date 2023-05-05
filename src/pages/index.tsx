6e9e8498689ebb53763a8bfbc9769b8f05e5da6b
imaimai17468 <tosiki_801_life@outlook.jp>
2023-05-06 03:08:50 +0900

    [fix] トップページ大改修

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,54 @@
-import { motion } from 'framer-motion'
+import { motion, useScroll, useSpring } from 'framer-motion'
+import { NextPage } from 'next'
 import Head from 'next/head'
-import React, { useRef } from 'react'
+import Image from 'next/image'
+import React, { useRef, useMemo } from 'react'
+import { BsCaretRightFill, BsMouseFill } from 'react-icons/bs'
+import { FaBirthdayCake } from 'react-icons/fa'
+import { GiMaterialsScience, GiPaintBrush } from 'react-icons/gi'
+import { SiTwitter, SiGithub } from 'react-icons/si'
 import { useRecoilValue } from 'recoil'
 
 import { MainLayout } from '@/components/layout'
 import { BackgroundAnimation } from '@/components/screen'
 import { useVariants, spring } from '@/hooks/useVariants'
 import { cursorState } from '@/store/cursor'
+import { frontendIcons, backendIcons, otherIcons, toolIcons } from '@/utils/icons'
+import { containerMotion, childMotion } from '@/utils/motions'
 
-export default function Home() {
+const IndexPage: NextPage = () => {
   const cursor = useRecoilValue(cursorState)
   const ref = useRef(null)
   const variants = useVariants(ref)
+  const { scrollYProgress } = useScroll()
+  const scaleY = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  })
+
+  const timeText = useMemo(() => {
+    const date = new Date()
+    const hour = date.getHours()
+    if (hour >= 5 && hour < 12) {
+      return 'Good Morning!!'
+    }
+    if (hour >= 12 && hour < 18) {
+      return 'Good Afternoon!!'
+    }
+    return 'Good Evening!!'
+  }, [])
 
   return (
-    <>
+    <html lang='ja' className='scroll-smooth'>
       <Head>
         <title>imaimai Portfolio</title>
         <meta name='description' content='いまいまいのポートフォリオ' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/images/frog_circle.png' />
       </Head>
-      <main className='font-mono text-gray-200'>
-        <div ref={ref}>
+      <main ref={ref} className='scroll-smooth font-mono text-gray-200'>
+        <div>
           <MainLayout>
             <BackgroundAnimation />
             <motion.div
@@ -33,9 +59,201 @@ export default function Home() {
             >
               <div>{cursor.text}</div>
             </motion.div>
+            <div className='hidden md:block'>
+              <motion.div className='fixed bottom-0 left-5 top-0 w-1 origin-top bg-emerald-400' style={{ scaleY }} />
+              <motion.div className='fixed bottom-0 left-4 top-0 w-0.5 bg-emerald-500' style={{ scaleY }} />
+              <motion.div
+                className='fixed bottom-0 left-3 top-0 w-0.5 origin-bottom bg-emerald-600'
+                style={{ scaleY }}
+              />
+              <div className='fixed left-10 my-10 flex flex-col gap-10 text-2xl font-thin text-gray-200'>
+                <a href=' #top' className='flex items-center gap-2'>
+                  <BsCaretRightFill className='text-emerald-400' />
+                  <span>Top</span>
+                </a>
+                <a href=' #about' className='flex items-center gap-2'>
+                  <BsCaretRightFill className='text-emerald-400' />
+                  <span>About</span>
+                </a>
+                <a href=' #skill' className='flex items-center gap-2'>
+                  <BsCaretRightFill className='text-emerald-400' />
+                  <span>Skill</span>
+                </a>
+              </div>
+            </div>
+            <div
+              id='top'
+              className='flex h-screen -translate-y-24 flex-col items-center justify-center text-2xl font-thin text-gray-200 md:text-5xl'
+            >
+              <div className='mb-10 w-fit bg-background bg-opacity-70 p-5 md:p-10'>
+                <h1>{timeText}</h1>
+                <div className='flex gap-10'>
+                  <h1>I&apos;m</h1>
+                  <motion.div
+                    className='flex text-emerald-400'
+                    variants={containerMotion}
+                    initial='hidden'
+                    animate='visible'
+                  >
+                    {Array.from('imaimai.').map((char, i) => (
+                      <motion.h1 key={i} variants={childMotion}>
+                        {char}
+                      </motion.h1>
+                    ))}
+                  </motion.div>
+                </div>
+                <h1>Front End Developer</h1>
+              </div>
+              <div className='flex items-center gap-3 text-2xl'>
+                <p>Scroll Down</p>
+                <BsMouseFill className='animate-bounce' />
+              </div>
+            </div>
+            <div className='my-10 flex flex-col items-center gap-20'>
+              <div className='w-9/10 bg-background bg-opacity-70 p-10 lg:w-1/2'>
+                <h2 id='about' className='w-fit border-b-2 border-emerald-400 text-2xl'>
+                  About
+                </h2>
+                <div className='my-5 flex flex-col items-center justify-between md:flex-row md:gap-20'>
+                  <div>
+                    <div className='flex items-center gap-5'>
+                      <motion.div
+                        className='flex text-3xl'
+                        variants={containerMotion}
+                        initial='hidden'
+                        whileInView='visible'
+                      >
+                        {Array.from('Toshiki_Imai').map((char, i) => (
+                          <motion.h3 key={i} variants={childMotion}>
+                            {char}
+                          </motion.h3>
+                        ))}
+                      </motion.div>
+                      <div className='flex gap-2'>
+                        <a href='https://twitter.com/imaimai17468' target='_blank' rel='noopener noreferrer'>
+                          <SiTwitter className='text-xl text-emerald-400 transition-all hover:text-emerald-300' />
+                        </a>
+                        <a href='https://github.com/imaimai17468' target='_blank' rel='noopener noreferrer'>
+                          <SiGithub className='text-xl text-emerald-400 transition-all hover:text-emerald-300' />
+                        </a>
+                      </div>
+                    </div>
+                    <p className='text-gray-400'>長岡技術科学大学 電気電子情報工学課程 4年</p>
+                    <p className='text-gray-400'>フロントエンドエンジニア</p>
+                    <motion.div className='my-3' variants={containerMotion} initial='hidden' whileInView='visible'>
+                      <motion.div className='flex items-center gap-3' variants={childMotion}>
+                        <GiMaterialsScience />
+                        <span>脳情報工学研究室</span>
+                      </motion.div>
+                      <motion.div className='flex items-center gap-3' variants={childMotion}>
+                        <FaBirthdayCake />
+                        <span>2001/08/01</span>
+                      </motion.div>
+                      <motion.div className='flex items-center gap-3' variants={childMotion}>
+                        <GiPaintBrush />
+                        <span>開発・料理・MCバトル観戦</span>
+                      </motion.div>
+                    </motion.div>
+                  </div>
+                  <Image
+                    alt='profile'
+                    src='/images/frog_circle.png'
+                    width={200}
+                    height={200}
+                    className='rounded-full'
+                  />
+                </div>
+              </div>
+              <div className='w-9/10 bg-background bg-opacity-70 p-10 lg:w-1/2'>
+                <h2 id='skill' className='w-fit border-b-2 border-emerald-400 text-2xl'>
+                  Skill
+                </h2>
+                <div className='my-5'>
+                  <p className='text-xl font-thin text-emerald-400'>Front End</p>
+                  <motion.div
+                    className='my-5 grid grid-cols-3 justify-items-center gap-y-5 text-4xl md:grid-cols-6 md:gap-y-10'
+                    variants={containerMotion}
+                    initial='hidden'
+                    whileInView='visible'
+                  >
+                    {frontendIcons.map((skill, i) => (
+                      <div
+                        key={i}
+                        className='flex flex-col items-center justify-center transition-all hover:text-emerald-400'
+                      >
+                        {skill.icon}
+                        <p className='mt-2 text-center text-sm font-thin'>{skill.name}</p>
+                      </div>
+                    ))}
+                  </motion.div>
+                </div>
+                <hr className='border-dotted border-gray-400' />
+                <div className='my-5'>
+                  <p className='text-xl font-thin text-emerald-400'>Back End</p>
+                  <motion.div
+                    className='my-5 grid grid-cols-3 justify-items-center gap-y-5 text-4xl md:grid-cols-6 md:gap-y-10'
+                    variants={containerMotion}
+                    initial='hidden'
+                    whileInView='visible'
+                  >
+                    {backendIcons.map((skill, i) => (
+                      <div
+                        key={i}
+                        className='flex flex-col items-center justify-center transition-all hover:text-emerald-400'
+                      >
+                        {skill.icon}
+                        <p className='mt-2 text-center text-sm font-thin'>{skill.name}</p>
+                      </div>
+                    ))}
+                  </motion.div>
+                </div>
+                <hr className='border-dotted border-gray-400' />
+                <div className='my-5'>
+                  <p className='text-xl font-thin text-emerald-400'>Other</p>
+                  <motion.div
+                    className='my-5 grid grid-cols-3 justify-items-center gap-y-5 text-4xl md:grid-cols-6 md:gap-y-10'
+                    variants={containerMotion}
+                    initial='hidden'
+                    whileInView='visible'
+                  >
+                    {otherIcons.map((skill, i) => (
+                      <div
+                        key={i}
+                        className='flex flex-col items-center justify-center transition-all hover:text-emerald-400'
+                      >
+                        {skill.icon}
+                        <p className='mt-2 text-center text-sm font-thin'>{skill.name}</p>
+                      </div>
+                    ))}
+                  </motion.div>
+                </div>
+                <hr className='border-dotted border-gray-400' />
+                <div className='my-5'>
+                  <p className='text-xl font-thin text-emerald-400'>Tools</p>
+                  <motion.div
+                    className='my-5 grid grid-cols-3 justify-items-center gap-y-5 text-4xl md:grid-cols-6 md:gap-y-10'
+                    variants={containerMotion}
+                    initial='hidden'
+                    whileInView='visible'
+                  >
+                    {toolIcons.map((skill, i) => (
+                      <div
+                        key={i}
+                        className='flex flex-col items-center justify-center transition-all hover:text-emerald-400'
+                      >
+                        {skill.icon}
+                        <p className='mt-2 text-center text-sm font-thin'>{skill.name}</p>
+                      </div>
+                    ))}
+                  </motion.div>
+                </div>
+              </div>
+            </div>
           </MainLayout>
         </div>
       </main>
-    </>
+    </html>
   )
 }
+
+export default IndexPage
