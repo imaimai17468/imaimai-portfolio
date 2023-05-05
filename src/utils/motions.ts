a60e2f73205f1775dda4e5d160d80e3134e6304c
imaimai17468 <tosiki_801_life@outlook.jp>
2023-05-06 03:09:28 +0900

    [feat] motion一覧を定義

---
+++ src/utils/motions.ts
@@ -0,0 +1,30 @@
+export const containerMotion = {
+  hidden: { opacity: 0 },
+  visible: (i = 1) => ({
+    opacity: 1,
+    transition: { staggerChildren: 0.2, delayChildren: 0.04 * i },
+  }),
+}
+
+export const childMotion = {
+  visible: {
+    opacity: 1,
+    x: 0,
+    y: 0,
+    transition: {
+      type: 'spring',
+      damping: 12,
+      stiffness: 100,
+    },
+  },
+  hidden: {
+    opacity: 0,
+    x: -20,
+    y: 10,
+    transition: {
+      type: 'spring',
+      damping: 12,
+      stiffness: 100,
+    },
+  },
+}
