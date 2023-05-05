385c4fe6dbc45bbb625f21b41c157835a306dcd5
imaimai17468 <tosiki_801_life@outlook.jp>
2023-05-06 03:09:08 +0900

    [feat] icon一覧の定義を追加

---
+++ src/utils/icons.tsx
@@ -0,0 +1,64 @@
+import {
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiTypescript,
+  SiReact,
+  SiNextdotjs,
+  SiNuxtdotjs,
+  SiTailwindcss,
+  SiMaterialui,
+  SiSass,
+  SiRubyonrails,
+  SiGo,
+  SiFirebase,
+  SiProcessingfoundation,
+  SiSwagger,
+  SiC,
+  SiCplusplus,
+  SiPython,
+  SiScikitlearn,
+  SiArduino,
+  SiUnity,
+  SiDocker,
+  SiGit,
+  SiGithub,
+  SiFigma,
+} from 'react-icons/si'
+
+export const frontendIcons = [
+  { icon: <SiHtml5 />, name: 'HTML5' },
+  { icon: <SiCss3 />, name: 'CSS3' },
+  { icon: <SiJavascript />, name: 'JavaScript' },
+  { icon: <SiTypescript />, name: 'TypeScript' },
+  { icon: <SiReact />, name: 'React' },
+  { icon: <SiNextdotjs />, name: 'Next.js' },
+  { icon: <SiNuxtdotjs />, name: 'Nuxt.js' },
+  { icon: <SiTailwindcss />, name: 'Tailwind CSS' },
+  { icon: <SiMaterialui />, name: 'Material-UI' },
+  { icon: <SiSass />, name: 'Sass' },
+  { icon: <SiProcessingfoundation />, name: 'p5.js' },
+]
+
+export const backendIcons = [
+  { icon: <SiRubyonrails />, name: 'Ruby on Rails' },
+  { icon: <SiGo />, name: 'Go' },
+  { icon: <SiSwagger />, name: 'Swagger' },
+  { icon: <SiFirebase />, name: 'Firebase' },
+]
+
+export const otherIcons = [
+  { icon: <SiC />, name: 'C' },
+  { icon: <SiCplusplus />, name: 'C++' },
+  { icon: <SiPython />, name: 'Python' },
+  { icon: <SiScikitlearn />, name: 'scikit-learn' },
+  { icon: <SiArduino />, name: 'Arduino' },
+  { icon: <SiUnity />, name: 'Unity' },
+  { icon: <SiDocker />, name: 'Docker' },
+]
+
+export const toolIcons = [
+  { icon: <SiGit />, name: 'Git' },
+  { icon: <SiGithub />, name: 'GitHub' },
+  { icon: <SiFigma />, name: 'Figma' },
+]
