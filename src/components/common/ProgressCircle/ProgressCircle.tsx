91c936850009414163d197a51f5f719efcc767ab
imaimai17468 <tosiki_801_life@outlook.jp>
2023-05-07 01:27:12 +0900

    [feat] add progress circle

---
+++ src/components/common/ProgressCircle/ProgressCircle.tsx
@@ -0,0 +1,21 @@
+import { motion } from 'framer-motion'
+
+import { ProgressCircleProps } from './ProgressCircle.types'
+
+export const ProgressCircle: React.FC<ProgressCircleProps> = ({ progress }: ProgressCircleProps) => (
+  <figure>
+    <svg id='progress' width='50' height='50' viewBox='0 0 100 100'>
+      <circle cx='50' cy='50' r='20' className='stroke fill-emerald-400 stroke-2' />
+      <motion.circle
+        cx='50'
+        cy='50'
+        r='30'
+        pathLength='1'
+        className='stroke fill-none stroke-emerald-400 stroke-2'
+        style={{ pathLength: progress }}
+      />
+    </svg>
+  </figure>
+)
+
+export default ProgressCircle
---
+++ src/components/common/ProgressCircle/ProgressCircle.types.ts
@@ -0,0 +1,5 @@
+import { MotionValue } from 'framer-motion'
+
+export interface ProgressCircleProps {
+  progress: MotionValue<number>
+}
