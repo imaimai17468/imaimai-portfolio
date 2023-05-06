8ad3a9470b6af6280589a47653e05e3d94b88b0f
imaimai17468 <tosiki_801_life@outlook.jp>
2023-05-07 02:10:06 +0900

    [feat] useMouse実装

---
+++ src/hooks/useMouse.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+
+const useMouse = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const updatePosition = (e: MouseEvent) => {
+      setPosition({ x: e.clientX, y: e.clientY })
+    }
+
+    window.addEventListener('mousemove', updatePosition)
+
+    return () => {
+      window.removeEventListener('mousemove', updatePosition)
+    }
+  }, [])
+
+  return position
+}
+
+export default useMouse
