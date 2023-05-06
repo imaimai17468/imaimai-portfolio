ad6c4abe2a29582bc63fb6a46f2cc8208ead5996
imaimai17468 <tosiki_801_life@outlook.jp>
2023-05-07 01:28:29 +0900

    [feat] progress barにそれぞれの進み具合を表す円を追加

--- src/components/screen/ProgressBar/ProgressBar.tsx
+++ src/components/screen/ProgressBar/ProgressBar.tsx
@@ -1,8 +1,23 @@
 import { motion, useScroll, useSpring } from 'framer-motion'
-import { BsCaretRightFill } from 'react-icons/bs'
 
-export const ProgressBar = () => {
+import { ProgressCircle } from '@/components/common'
+
+import { ProgressBarProps } from './ProgressBar.types'
+
+export const ProgressBar: React.FC<ProgressBarProps> = ({ aboutRef, topRef, skillsRef }: ProgressBarProps) => {
   const { scrollYProgress } = useScroll()
+  const { scrollYProgress: topScrollYProgress } = useScroll({
+    target: topRef,
+    offset: ['end start', 'start'],
+  })
+  const { scrollYProgress: aboutScrollYProgress } = useScroll({
+    target: aboutRef,
+    offset: ['end', 'start'],
+  })
+  const { scrollYProgress: skillsScrollYProgress } = useScroll({
+    target: skillsRef,
+    offset: ['start end', 'end'],
+  })
   const scaleY = useSpring(scrollYProgress, {
     stiffness: 100,
     damping: 30,
@@ -15,16 +30,16 @@ export const ProgressBar = () => {
       <motion.div className='fixed bottom-0 left-4 top-0 w-0.5 bg-emerald-500' style={{ scaleY }} />
       <motion.div className='fixed bottom-0 left-3 top-0 w-0.5 origin-bottom bg-emerald-600' style={{ scaleY }} />
       <div className='fixed left-10 z-10 my-10 flex flex-col gap-10 text-2xl font-thin text-gray-200'>
-        <a href=' #top' className='flex items-center gap-2'>
-          <BsCaretRightFill className='text-emerald-400' />
+        <a href=' #top' className='flex items-center'>
+          <ProgressCircle progress={topScrollYProgress} />
           <span className='transition-all hover:text-emerald-400'>Top</span>
         </a>
-        <a href=' #about' className='flex items-center gap-2'>
-          <BsCaretRightFill className='text-emerald-400' />
+        <a href=' #about' className='flex items-center'>
+          <ProgressCircle progress={aboutScrollYProgress} />
           <span className='transition-all hover:text-emerald-400'>About</span>
         </a>
-        <a href=' #skill' className='flex items-center gap-2'>
-          <BsCaretRightFill className='text-emerald-400' />
+        <a href=' #skill' className='flex items-center'>
+          <ProgressCircle progress={skillsScrollYProgress} />
           <span className='transition-all hover:text-emerald-400'>Skill</span>
         </a>
       </div>
---
+++ src/components/screen/ProgressBar/ProgressBar.types.ts
@@ -0,0 +1,5 @@
+export interface ProgressBarProps {
+  topRef: React.RefObject<HTMLDivElement>
+  aboutRef: React.RefObject<HTMLDivElement>
+  skillsRef: React.RefObject<HTMLDivElement>
+}
