c2c692c630afea20b1cb0e622697d5cce3f67545
imaimai17468 <tosiki_801_life@outlook.jp>
2023-05-07 15:27:59 +0900

    [fix] headを削除してappに移動

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import { Analytics } from '@vercel/analytics/react'
+import Head from 'next/head'
 import { RecoilRoot } from 'recoil'
 
 import type { AppProps } from 'next/app'
@@ -7,6 +8,12 @@ import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>imaimai Portfolio</title>
+        <meta name='description' content='いまいまいのポートフォリオ' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/images/frog_circle.png' />
+      </Head>
       <Analytics />
       <RecoilRoot>
         <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { motion } from 'framer-motion'
 import { NextPage } from 'next'
-import Head from 'next/head'
 import Image from 'next/image'
 import React, { useRef, useMemo } from 'react'
 import { AiOutlineLink } from 'react-icons/ai'
@@ -52,126 +51,112 @@ const IndexPage: NextPage = () => {
   }
 
   return (
-    <>
-      <Head>
-        <title>imaimai Portfolio</title>
-        <meta name='description' content='いまいまいのポートフォリオ' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/images/frog_circle.png' />
-      </Head>
-      <main className='font-mono text-gray-200'>
-        <div>
-          <MainLayout>
-            <BackgroundAnimation />
-            <motion.div
-              variants={variants}
-              style={{ position: 'fixed' }}
-              className='invisible z-50 flex items-center justify-center rounded-full md:visible'
-              animate={cursor.variant}
-              transition={spring}
-            >
-              <div>{cursor.text}</div>
-            </motion.div>
-            <ProgressBar topRef={topRef} aboutRef={aboutRef} skillsRef={skillRef} />
-            <div
-              id='top'
-              className='flex h-screen -translate-y-24 flex-col items-center justify-center text-2xl font-thin text-gray-200 md:text-5xl'
-              ref={topRef}
-            >
-              <div className='mb-10 w-fit bg-background bg-opacity-70 p-5 md:p-10'>
-                <h1>{timeText}</h1>
-                <div className='flex gap-10'>
-                  <h1>I&apos;m</h1>
-                  <CharAnimation char='imaimai.' className='text-emerald-500' />
-                </div>
-                <h1>Front End Developer</h1>
+    <main className='font-mono text-gray-200'>
+      <div>
+        <MainLayout>
+          <BackgroundAnimation />
+          <motion.div
+            variants={variants}
+            style={{ position: 'fixed' }}
+            className='invisible z-50 flex items-center justify-center rounded-full md:visible'
+            animate={cursor.variant}
+            transition={spring}
+          >
+            <div>{cursor.text}</div>
+          </motion.div>
+          <ProgressBar topRef={topRef} aboutRef={aboutRef} skillsRef={skillRef} />
+          <div
+            id='top'
+            className='flex h-screen -translate-y-24 flex-col items-center justify-center text-2xl font-thin text-gray-200 md:text-5xl'
+            ref={topRef}
+          >
+            <div className='mb-10 w-fit bg-background bg-opacity-70 p-5 md:p-10'>
+              <h1>{timeText}</h1>
+              <div className='flex gap-10'>
+                <h1>I&apos;m</h1>
+                <CharAnimation char='imaimai.' className='text-emerald-500' />
               </div>
-              <div className='flex flex-col items-center gap-3 text-2xl'>
-                <p>Scroll Down</p>
-                <BsMouseFill className='animate-bounce' />
-              </div>
+              <h1>Front End Developer</h1>
             </div>
-            <div className='my-10 flex flex-col items-center gap-20'>
-              <div id='about' className='w-9/10 bg-background bg-opacity-70 p-10 lg:w-1/2' ref={aboutRef}>
-                <h2 className='w-fit border-b-2 border-emerald-400 text-2xl'>About</h2>
-                <div className='my-5 flex flex-col items-center justify-between md:flex-row md:gap-20'>
-                  <div>
-                    <div className='flex items-center gap-5'>
-                      <CharAnimation char='Toshiki_Imai' className='text-3xl' />
-                      <div className='flex gap-2'>
-                        <a
-                          href='https://twitter.com/imaimai17468'
-                          target='_blank'
-                          rel='noopener noreferrer'
-                          onMouseEnter={cursorChange2Link}
-                          onMouseLeave={cursorChange2Default}
-                        >
-                          <SiTwitter className='text-xl text-emerald-400 transition-all hover:text-emerald-300' />
-                        </a>
-                        <a
-                          href='https://github.com/imaimai17468'
-                          target='_blank'
-                          rel='noopener noreferrer'
-                          onMouseEnter={cursorChange2Link}
-                          onMouseLeave={cursorChange2Default}
-                        >
-                          <SiGithub className='text-xl text-emerald-400 transition-all hover:text-emerald-300' />
-                        </a>
-                      </div>
+            <div className='flex flex-col items-center gap-3 text-2xl'>
+              <p>Scroll Down</p>
+              <BsMouseFill className='animate-bounce' />
+            </div>
+          </div>
+          <div className='my-10 flex flex-col items-center gap-20'>
+            <div id='about' className='w-9/10 bg-background bg-opacity-70 p-10 lg:w-1/2' ref={aboutRef}>
+              <h2 className='w-fit border-b-2 border-emerald-400 text-2xl'>About</h2>
+              <div className='my-5 flex flex-col items-center justify-between md:flex-row md:gap-20'>
+                <div>
+                  <div className='flex items-center gap-5'>
+                    <CharAnimation char='Toshiki_Imai' className='text-3xl' />
+                    <div className='flex gap-2'>
+                      <a
+                        href='https://twitter.com/imaimai17468'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        onMouseEnter={cursorChange2Link}
+                        onMouseLeave={cursorChange2Default}
+                      >
+                        <SiTwitter className='text-xl text-emerald-400 transition-all hover:text-emerald-300' />
+                      </a>
+                      <a
+                        href='https://github.com/imaimai17468'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        onMouseEnter={cursorChange2Link}
+                        onMouseLeave={cursorChange2Default}
+                      >
+                        <SiGithub className='text-xl text-emerald-400 transition-all hover:text-emerald-300' />
+                      </a>
                     </div>
-                    <p className='text-gray-400'>長岡技術科学大学 電気電子情報工学課程 4年</p>
-                    <p className='text-gray-400'>フロントエンドエンジニア</p>
-                    <motion.div className='my-3' variants={containerMotion} initial='hidden' whileInView='visible'>
-                      <motion.div className='flex items-center gap-3' variants={childMotion}>
-                        <GiMaterialsScience />
-                        <span>脳情報工学研究室</span>
-                      </motion.div>
-                      <motion.div className='flex items-center gap-3' variants={childMotion}>
-                        <FaBirthdayCake />
-                        <span>2001/08/01</span>
-                      </motion.div>
-                      <motion.div className='flex items-center gap-3' variants={childMotion}>
-                        <GiPaintBrush />
-                        <span>開発・料理・MCバトル観戦</span>
-                      </motion.div>
-                    </motion.div>
                   </div>
-                  <Image
-                    alt='profile'
-                    src='/images/frog_circle.png'
-                    width={200}
-                    height={200}
-                    className='rounded-full'
-                  />
+                  <p className='text-gray-400'>長岡技術科学大学 電気電子情報工学課程 4年</p>
+                  <p className='text-gray-400'>フロントエンドエンジニア</p>
+                  <motion.div className='my-3' variants={containerMotion} initial='hidden' whileInView='visible'>
+                    <motion.div className='flex items-center gap-3' variants={childMotion}>
+                      <GiMaterialsScience />
+                      <span>脳情報工学研究室</span>
+                    </motion.div>
+                    <motion.div className='flex items-center gap-3' variants={childMotion}>
+                      <FaBirthdayCake />
+                      <span>2001/08/01</span>
+                    </motion.div>
+                    <motion.div className='flex items-center gap-3' variants={childMotion}>
+                      <GiPaintBrush />
+                      <span>開発・料理・MCバトル観戦</span>
+                    </motion.div>
+                  </motion.div>
                 </div>
-              </div>
-              <div id='skill' className='w-9/10 bg-background bg-opacity-70 p-10 lg:w-1/2' ref={skillRef}>
-                <h2 className='w-fit border-b-2 border-emerald-400 text-2xl'>Skill</h2>
-                <div className='my-5'>
-                  <p className='text-xl font-thin text-emerald-400'>Front End</p>
-                  <IconList icons={frontendIcons} />
-                </div>
-                <hr className='border-dotted border-gray-400' />
-                <div className='my-5'>
-                  <p className='text-xl font-thin text-emerald-400'>Back End</p>
-                  <IconList icons={backendIcons} />
-                </div>
-                <hr className='border-dotted border-gray-400' />
-                <div className='my-5'>
-                  <p className='text-xl font-thin text-emerald-400'>Other</p>
-                  <IconList icons={otherIcons} />
-                </div>
-                <hr className='border-dotted border-gray-400' />
-                <div className='my-5'>
-                  <p className='text-xl font-thin text-emerald-400'>Tools</p>
-                  <IconList icons={toolIcons} />
-                </div>
+                <Image alt='profile' src='/images/frog_circle.png' width={200} height={200} className='rounded-full' />
               </div>
             </div>
-          </MainLayout>
-        </div>
-      </main>
-    </>
+            <div id='skill' className='w-9/10 bg-background bg-opacity-70 p-10 lg:w-1/2' ref={skillRef}>
+              <h2 className='w-fit border-b-2 border-emerald-400 text-2xl'>Skill</h2>
+              <div className='my-5'>
+                <p className='text-xl font-thin text-emerald-400'>Front End</p>
+                <IconList icons={frontendIcons} />
+              </div>
+              <hr className='border-dotted border-gray-400' />
+              <div className='my-5'>
+                <p className='text-xl font-thin text-emerald-400'>Back End</p>
+                <IconList icons={backendIcons} />
+              </div>
+              <hr className='border-dotted border-gray-400' />
+              <div className='my-5'>
+                <p className='text-xl font-thin text-emerald-400'>Other</p>
+                <IconList icons={otherIcons} />
+              </div>
+              <hr className='border-dotted border-gray-400' />
+              <div className='my-5'>
+                <p className='text-xl font-thin text-emerald-400'>Tools</p>
+                <IconList icons={toolIcons} />
+              </div>
+            </div>
+          </div>
+        </MainLayout>
+      </div>
+    </main>
   )
 }
 
