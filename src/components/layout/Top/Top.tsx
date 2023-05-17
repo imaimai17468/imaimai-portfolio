a1c2aad5e7f2a96322667c63a109fc65ca11be92
imaimai17468 <tosiki_801_life@outlook.jp>
2023-05-18 03:03:03 +0900

    [fix] Top周りをコンポーネントとして切り出し

---
+++ src/components/layout/Top/Top.tsx
@@ -0,0 +1,43 @@
+import { useMemo } from 'react'
+import { BsMouseFill } from 'react-icons/bs'
+
+import { CharAnimation } from '@/components/common'
+
+import { TopProps } from './Top.types'
+
+const Top: React.FC<TopProps> = ({ topRef }: TopProps) => {
+  const timeText = useMemo(() => {
+    const date = new Date()
+    const hour = date.getHours()
+    if (hour >= 5 && hour < 12) {
+      return 'Good Morning!!'
+    }
+    if (hour >= 12 && hour < 18) {
+      return 'Good Afternoon!!'
+    }
+    return 'Good Evening!!'
+  }, [])
+
+  return (
+    <div
+      id='top'
+      className='flex h-screen -translate-y-12 flex-col items-center justify-center text-2xl font-thin text-gray-200 md:text-5xl'
+      ref={topRef}
+    >
+      <div className='mb-10 w-fit bg-background bg-opacity-70 p-5 md:p-10'>
+        <h1>{timeText}</h1>
+        <div className='flex gap-10'>
+          <h1>I&apos;m</h1>
+          <CharAnimation char='imaimai.' className='text-emerald-500' />
+        </div>
+        <h1>Front End Developer</h1>
+      </div>
+      <div className='flex flex-col items-center gap-3 text-2xl'>
+        <p>Scroll Down</p>
+        <BsMouseFill className='animate-bounce' />
+      </div>
+    </div>
+  )
+}
+
+export default Top
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import { NextPage } from 'next'
-import React, { useRef, useMemo } from 'react'
-import { BsMouseFill } from 'react-icons/bs'
+import React, { useRef } from 'react'
 
-import { CharAnimation } from '@/components/common'
-import { IconList, About } from '@/components/layout'
+import { IconList, About, Top } from '@/components/layout'
 import { ProgressBar } from '@/components/screen'
 import { frontendIcons, backendIcons, otherIcons, toolIcons } from '@/utils/icons'
 
@@ -12,40 +10,11 @@ const IndexPage: NextPage = () => {
   const aboutRef = useRef(null)
   const skillRef = useRef(null)
 
-  const timeText = useMemo(() => {
-    const date = new Date()
-    const hour = date.getHours()
-    if (hour >= 5 && hour < 12) {
-      return 'Good Morning!!'
-    }
-    if (hour >= 12 && hour < 18) {
-      return 'Good Afternoon!!'
-    }
-    return 'Good Evening!!'
-  }, [])
-
   return (
     <main className='font-mono text-gray-200'>
       <ProgressBar topRef={topRef} aboutRef={aboutRef} skillsRef={skillRef} />
       <div className='my-10 flex flex-col items-center gap-20'>
-        <div
-          id='top'
-          className='flex h-screen -translate-y-12 flex-col items-center justify-center text-2xl font-thin text-gray-200 md:text-5xl'
-          ref={topRef}
-        >
-          <div className='mb-10 w-fit bg-background bg-opacity-70 p-5 md:p-10'>
-            <h1>{timeText}</h1>
-            <div className='flex gap-10'>
-              <h1>I&apos;m</h1>
-              <CharAnimation char='imaimai.' className='text-emerald-500' />
-            </div>
-            <h1>Front End Developer</h1>
-          </div>
-          <div className='flex flex-col items-center gap-3 text-2xl'>
-            <p>Scroll Down</p>
-            <BsMouseFill className='animate-bounce' />
-          </div>
-        </div>
+        <Top topRef={topRef} />
         <About aboutRef={aboutRef} />
         <div id='skill' className='w-9/10 bg-background bg-opacity-70 p-10 lg:w-3/5 xl:w-1/2' ref={skillRef}>
           <h2 className='w-fit border-b-2 border-emerald-400 text-2xl'>Skill</h2>
