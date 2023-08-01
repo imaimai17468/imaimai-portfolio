10a4caa3cb4781693def83e12c803b8ea3d03efd
imaimai17468 <tosiki_801_life@outlook.jp>
2023-08-01 19:40:25 +0900

    [feat] contactページの送信処理が実装中だと明記

--- src/components/layout/Contact/Contact.tsx
+++ src/components/layout/Contact/Contact.tsx
@@ -23,7 +23,7 @@ const Contact: React.FC<ContactProps> = ({ contactRef }: ContactProps) => {
 
   return (
     <div id='contact' className='w-9/10 bg-background bg-opacity-70 p-10 lg:w-3/5 xl:w-1/2' ref={contactRef}>
-      <h2 className='w-fit border-b-2 border-emerald-400 text-2xl'>Contact</h2>
+      <h2 className='w-fit border-b-2 border-emerald-400 text-2xl'>Contact: 実装中</h2>
       <form onSubmit={handleSubmit(onSubmit, console.error)}>
         <div className='my-8 flex flex-col items-center gap-16'>
           <div className=' flex w-full flex-col gap-8'>
@@ -61,7 +61,10 @@ const Contact: React.FC<ContactProps> = ({ contactRef }: ContactProps) => {
               {errors.message && <span className='text-red-500'>この項目は必須です</span>}
             </div>
           </div>
-          <Button className='w-32' disabled={!isValid || isSubmitting}>
+          {/* <Button className='w-32' disabled={!isValid || isSubmitting}>
+            Submit
+          </Button> */}
+          <Button className='w-32' disabled>
             Submit
           </Button>
         </div>
