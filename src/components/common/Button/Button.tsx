3a08f208e8537b40aaa2bb30e9856f45f524820d
imaimai17468 <tosiki_801_life@outlook.jp>
2023-08-01 19:26:24 +0900

    [fix] buttonにdisabledを追加

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -9,21 +9,23 @@ const Button: React.FC<ButtonProps> = ({
   onClick,
   onMouseEnter,
   onMouseLeave,
+  disabled,
 }: ButtonProps) => (
   <motion.button
-    className={`rounded-full bg-gradient-to-r px-4 py-2 font-bold text-gray-100 ${s.shine} ${className}`}
+    className={`rounded-full bg-gradient-to-r px-4 py-2 font-bold text-gray-100 disabled:opacity-50 ${s.shine} ${className}`}
     onClick={() => {
       if (onClick) onClick()
     }}
     whileHover={{ scale: 1.1 }}
     whileTap={{ scale: 0.9 }}
-    transition={{ type: 'spring', stiffness: 400, damping: 10 }}
+    transition={{ type: 'spring', stiffness: 100, damping: 10 }}
     onMouseEnter={() => {
       if (onMouseEnter) onMouseEnter()
     }}
     onMouseLeave={() => {
       if (onMouseLeave) onMouseLeave()
     }}
+    disabled={disabled}
   >
     {children}
   </motion.button>
--- src/components/common/Button/Button.types.ts
+++ src/components/common/Button/Button.types.ts
@@ -1,7 +1,7 @@
-export interface ButtonProps {
+export type ButtonProps = {
   children: React.ReactNode
   onClick?: () => void
   className?: string
   onMouseEnter?: () => void
   onMouseLeave?: () => void
-}
+} & JSX.IntrinsicElements['button']
