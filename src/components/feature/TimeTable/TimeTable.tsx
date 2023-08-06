a9e66b84a3483b29954ce0036b376ae694dc685d
imaimai17468 <tosiki_801_life@outlook.jp>
2023-08-07 00:11:16 +0900

    [fix] スマホ時のテーブルスタイルを修正

--- src/components/feature/TimeTable/TimeTable.tsx
+++ src/components/feature/TimeTable/TimeTable.tsx
@@ -1,23 +1,26 @@
 import { TimeTableProps } from './TimeTable.types'
 
 const TimeTable: React.FC<TimeTableProps> = ({ title, data }: TimeTableProps) => (
-  <div className='my-12 w-9/10 bg-background bg-opacity-70 p-10 lg:w-3/5 xl:w-1/2'>
+  <div className='my-12 w-9/10 bg-background bg-opacity-70 p-4 md:p-10 lg:w-3/5 xl:w-1/2'>
     <h1 className='w-fit border-b-2 border-emerald-400 text-2xl'>{title}</h1>
     <table className='my-5 w-full table-auto'>
       <thead>
         <tr>
-          <th className='px-4 py-2'>Date</th>
+          <th className='w-1/3 px-4 py-2'>Date</th>
           <th className='px-4 py-2'>Content</th>
         </tr>
       </thead>
       <tbody>
         {data.map((val) => (
-          <tr key={val.date} className='bg-background hover:bg-slate-300 hover:text-background'>
-            <td className='flex border-x border-emerald-400 px-4 py-2'>
+          <tr
+            key={val.date}
+            className='hover:text-background. border-l border-emerald-400 bg-background hover:bg-slate-300 hover:text-background'
+          >
+            <td className='flex h-fit p-2 md:px-4 md:py-2'>
               <p>{val.date}</p>
               <p className='ml-auto'>▷</p>
             </td>
-            <td className='px-4 py-2'>{val.content}</td>
+            <td className='border-l border-emerald-400 p-2 md:px-4 md:py-2'>{val.content}</td>
           </tr>
         ))}
       </tbody>
