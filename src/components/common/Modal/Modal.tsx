8e0c425f5033bc929e0b2b2f35e9fad451667e1b
imaimai17468 <tosiki_801_life@outlook.jp>
2023-09-10 02:51:40 +0900

    fix:fixed modal design

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -1,25 +1,29 @@
 import { ModalProps } from './Modal.type'
 
-export const Modal = ({ children, isOpen, title, onClose }: ModalProps) => (
+export const Modal = ({ children, title, onClose }: ModalProps) => (
   <div
-    className={`
-         absolute left-1/2 z-10 h-fit w-9/10 -translate-x-1/2 rounded-md border border-gray-200 bg-primary px-3 py-1 shadow-md shadow-gray-200 md:max-w-xl
-        ${isOpen ? 'opacity-100' : 'hidden opacity-0'}
-    `}
+    className='absolute left-1/2 z-50 flex h-full w-full -translate-x-1/2 -translate-y-1/2 items-center justify-center'
+    onClick={onClose}
   >
-    <div className='mb-1 flex flex-row items-center gap-3 border-b pb-1'>
-      <button
-        type='button'
-        className='flex h-8 w-8 items-center justify-center rounded-md border border-gray-200 active:bg-gray-200 active:text-background'
-        onClick={onClose}
-      >
-        <span className='mx-auto cursor-pointer text-xl' onClick={onClose}>
-          ×
-        </span>
-      </button>
-      <p className='text-xl'>{title}</p>
+    <div
+      className={`
+        w-9/10  rounded-md border border-emerald-400 bg-background px-3 py-1 shadow-md shadow-emerald-400
+    `}
+    >
+      <div className='flex flex-row items-center gap-3 border-b py-2'>
+        <button
+          type='button'
+          className='flex h-5 w-5 items-center justify-center rounded-md border border-emerald-400 active:bg-emerald-400 active:text-background'
+          onClick={onClose}
+        >
+          <span className='mx-auto cursor-pointer text-xl' onClick={onClose}>
+            ×
+          </span>
+        </button>
+        <p className='text-xl'>{title}</p>
+      </div>
+      <div className='p-3'>{children}</div>
     </div>
-    <div className='p-3'>{children}</div>
   </div>
 )
 
--- src/components/common/Modal/Modal.type.ts
+++ src/components/common/Modal/Modal.type.ts
@@ -1,6 +1,5 @@
 export interface ModalProps {
   children: React.ReactNode
-  isOpen: boolean
   onClose: () => void
   title: string
 }
