e7d9336bc4c5994221dfde372371c74aa8eb7bd9
imaimai17468 <tosiki_801_life@outlook.jp>
2024-03-09 22:14:04 +0900

    feat: ProductsCardコンポーネントを追加

---
+++ src/components/feature/ProductsCard/ProductsCard.tsx
@@ -0,0 +1,109 @@
+import { useState } from 'react'
+import { FiExternalLink } from 'react-icons/fi'
+import { RiSlideshowLine } from 'react-icons/ri'
+import { SiGithub, SiQiita } from 'react-icons/si'
+
+import { Product } from '@/@types/product'
+import { Modal } from '@/components/common'
+import { useCursor } from '@/hooks/useCursor'
+
+export const ProductCard: React.FC<{ product: Product }> = ({ product }: { product: Product }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const { cursorChange2Link, cursorChange2Default, cursorChange2Page } = useCursor()
+
+  return (
+    <>
+      {isModalOpen && (
+        <Modal title={product.name} onClose={() => setIsModalOpen(false)}>
+          <div className='flex flex-col gap-4 md:flex-row'>
+            {product.image && <img src={product.image} alt={product.name} className='w-full rounded-md md:w-1/2' />}
+            <div className='flex flex-col gap-2'>
+              <div className='flex flex-col gap-2'>
+                <p className='w-fit border-b border-emerald-400 font-bold'>開発人数</p>
+                <p>{product.popularity}人</p>
+              </div>
+              <div className='flex flex-col gap-2'>
+                <p className='w-fit border-b border-emerald-400 font-bold'>役割</p>
+                <p>{product.role}</p>
+              </div>
+              <div className='flex flex-col gap-2'>
+                <p className='w-fit border-b border-emerald-400 font-bold'>開発期間</p>
+                <p>{product.date}</p>
+              </div>
+              <div className='flex flex-col gap-2'>
+                <p className='w-fit border-b border-emerald-400 font-bold'>使用技術</p>
+                <div className='flex gap-2'>
+                  {product.techs.map((tech) => (
+                    <p key={tech}>{tech}</p>
+                  ))}
+                </div>
+              </div>
+              <div className='flex flex-col gap-2'>
+                <p className='w-fit border-b border-emerald-400 font-bold'>説明</p>
+                <p>{product.description}</p>
+              </div>
+              <div className='flex flex-col gap-2'>
+                <p className='w-fit border-b border-emerald-400 font-bold'>関連リンク</p>
+                <div className='flex gap-2'>
+                  {product.links?.github && (
+                    <a
+                      href={product.links?.github}
+                      className='cursor-pointer'
+                      aria-label='github'
+                      onMouseEnter={() => cursorChange2Link()}
+                      onMouseLeave={() => cursorChange2Default()}
+                    >
+                      <SiGithub size='2rem' />
+                    </a>
+                  )}
+                  {product.links?.product && (
+                    <a
+                      href={product.links?.product}
+                      className='cursor-pointer'
+                      aria-label='link'
+                      onMouseEnter={() => cursorChange2Link()}
+                      onMouseLeave={() => cursorChange2Default()}
+                    >
+                      <FiExternalLink size='2rem' />
+                    </a>
+                  )}
+                  {product.links?.slide && (
+                    <a
+                      href={product.links?.slide}
+                      className='cursor-pointer'
+                      aria-label='slide'
+                      onMouseEnter={() => cursorChange2Link()}
+                      onMouseLeave={() => cursorChange2Default()}
+                    >
+                      <RiSlideshowLine size='2rem' />
+                    </a>
+                  )}
+                  {product.links?.qiita && (
+                    <a
+                      href={product.links?.qiita}
+                      className='cursor-pointer'
+                      aria-label='qiita'
+                      onMouseEnter={() => cursorChange2Link()}
+                      onMouseLeave={() => cursorChange2Default()}
+                    >
+                      <SiQiita size='2rem' />
+                    </a>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </Modal>
+      )}
+      <button
+        type='button'
+        className='cursor-pointer break-words border border-emerald-400 p-2 text-sm md:text-xl'
+        onClick={() => setIsModalOpen(true)}
+        onMouseEnter={() => cursorChange2Page()}
+        onMouseLeave={() => cursorChange2Default()}
+      >
+        {product.logo ? <img src={product.logo} className='rounded-sm bg-gray-300 p-2' /> : <p>{product.name}</p>}
+      </button>
+    </>
+  )
+}
