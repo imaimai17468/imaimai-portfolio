4675d76852062ce61288a03a0ab52779d0dc833f
imaimai17468 <tosiki_801_life@outlook.jp>
2024-07-29 23:52:45 +0900

    fix: eventの内容を修正

--- src/constants/events.ts
+++ src/constants/events.ts
@@ -2,55 +2,19 @@ import { TimeTable } from '@/@types/timeTable'
 
 export const EVENTS = [
   {
-    date: '2019-8',
-    content: 'バンダイナムコスタジオ インターン',
+    date: '2022-7',
+    content: '第一回技育博にNUTMEGの代表として参加',
   },
   {
-    date: '2020-2023',
-    content: 'paiza株式会社 学習事業部 学生アルバイト',
+    date: '2023-10',
+    content: '技育展 決勝進出 企業賞受賞',
   },
   {
-    date: '2022~2024',
-    content: '長岡技術科学大学 技大祭実行委員会 情報局 (NUTMEG) に参加',
+    date: '2024-7',
+    content: '技育CAMPキャラバン@札幌にゆめみとして参加',
   },
   {
-    date: '2022-6',
-    content: '技育博 2022にNUTMEGの代表として参加',
-  },
-  {
-    date: '2022-8',
-    content: '学生団体 Tech Uniに参加',
-  },
-  {
-    date: '2022-9',
-    content: '学生団体 STECHに参加',
-  },
-  {
-    date: '2022-9',
-    content: '株式会社StyleArts 長期インターン',
-  },
-  {
-    date: '2022-10',
-    content: 'R&D株式会社 長期インターン',
-  },
-  {
-    date: '2022-12',
-    content: '株式会社ゆめみ 内定',
-  },
-  {
-    date: '2023-9',
-    content: '内定者アルバイトとして大型漫画サイトのリニューアルに参加',
-  },
-  {
-    date: '2023-9',
-    content: '技育展でNUTMEG-Seedsが企業賞を受賞',
-  },
-  {
-    date: '2024-1',
-    content: '内定者アルバイトとして大型ホームページのリニューアルに参加',
-  },
-  {
-    date: '2024-4',
-    content: '株式会社ゆめみ 入社',
+    date: '2024-8',
+    content: 'フロントエンドカンファレンス北海道 登壇',
   },
 ] as TimeTable[]
