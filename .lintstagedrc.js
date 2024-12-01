b83a7ab6be935ca3e1f8eb4e148ae7a1900264f6
imaimai17468 <tosiki_801_life@outlook.jp>
2024-12-01 10:34:02 +0900

    fix: eslint, prettierを削除

--- .lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  '**/*.{ts,tsx,css,scss}': (filenames) =>
-    `prettier --write ${filenames.map((file) => file.split(process.cwd())[1].slice(1)).join(' ')}`,
-  '**/*.{ts,tsx}': (filenames) =>
-    `eslint ${filenames.map((file) => file.split(process.cwd())[1].slice(1)).join(' ')} --fix`,
-}
